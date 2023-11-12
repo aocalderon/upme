7825bf9a14836c7e672800d209ad29e3edc7b72a
Andres Calderon <andress.calderon@gmail.com>
2023-11-12 08:17:57 -0800

    Setting dask configuration...

--- read_sentinel.py
+++ read_sentinel.py
@@ -46,67 +46,69 @@ def to_float(imagery):
     return _imagery.where(_imagery != nodata)
 
 if __name__ == "__main__":
-    with dask.distributed.LocalCluster(n_workers=int(args.workers), dashboard_address=':3258') as cluster:
+    with dask.distributed.LocalCluster(n_workers=int(args.workers), dashboard_address=':8100') as cluster:
         with dask.distributed.Client(cluster) as client:
-            configure_rio(cloud_defaults=True, verbose=True, client=client)
+            print(cluster)
+            print(client)
+            configure_rio(cloud_defaults=True, client=client)
     
-    catalog = Client.open("https://planetarycomputer.microsoft.com/api/stac/v1", modifier = pc.sign_inplace)
-    sids = []
+            catalog = Client.open("https://planetarycomputer.microsoft.com/api/stac/v1", modifier = pc.sign_inplace)
+            sids = []
 
-    # Define the path to the TSV file containing scene IDs...
-    input_file = args.file
-    sids_path = os.path.join(script_directory, input_file)
-    with open(input_file, 'r', newline='') as tsvfile:
-        tsvreader = csv.reader(tsvfile, delimiter='\t')
-        # Add scene IDs to the list...
-        for row in tsvreader:
-            sids.append(row[0])
+            # Define the path to the TSV file containing scene IDs...
+            input_file = args.file
+            sids_path = os.path.join(script_directory, input_file)
+            with open(input_file, 'r', newline='') as tsvfile:
+                tsvreader = csv.reader(tsvfile, delimiter='\t')
+                # Add scene IDs to the list...
+                for row in tsvreader:
+                    sids.append(row[0])
 
-    start = int(args.start)
+            start = int(args.start)
     
-    print(f"INFO\t{clocktime()}\tBULK\tDOWNLOAD\tSTART")
-    for sid in sids[start:]:
-        # Extract the year and month from the scene ID...
-        year_month = sid.split("_")[2][:6]
-        year = year_month[:4]
-        month = year_month[4:]
-        query =  catalog.search(
-            collections=["sentinel-2-l2a"], 
-            datetime=f"{year}-{month}", 
-            query ={"id":dict(eq=sid)}
-            )
-        items = list(query.items())
-        
-        # Check if the scene was found...
-        if len(items) != 0:
-            print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tFOUND")
-        
-            resolution = int(args.resolution)
-        
-            try:
-                imagery = stac_load(
-                    items, 
-                    #bands=["B02","B03","B04","B05","B06","B07","B08","B11","B12","SCL"],
-                    bands=["B12"],
-                    chunks={"x": 2048, "y": 2048}, 
-                    stac_cfg=cfg,
-                    patch_url = pc.sign,
-                    resolution=resolution,
-                )
-                bands = list(imagery.data_vars)
-        
-                print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tDOWNLOAD")
-                for band in bands:
-                    print(f"INFO\t{clocktime()}\t{sid}\t{band}\tSTART")
-                    output_path = os.path.join(output_folder, f"{sid}_{band}.tif")
-                    to_float(imagery.get(band)).compute().odc.write_cog(output_path, overwrite=False)
-                    print(f"INFO\t{clocktime()}\t{sid}\t{band}\tEND")
-                print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tDONE")
+            print(f"INFO\t{clocktime()}\tBULK\tDOWNLOAD\tSTART")
+            for sid in sids[start:]:
+                # Extract the year and month from the scene ID...
+                year_month = sid.split("_")[2][:6]
+                year = year_month[:4]
+                month = year_month[4:]
+                query =  catalog.search(
+                    collections=["sentinel-2-l2a"], 
+                    datetime=f"{year}-{month}", 
+                    query ={"id":dict(eq=sid)}
+                    )
+                items = list(query.items())
                 
-            except Exception as e:
-                print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tERROR")
-        else:
-            print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tNOT FOUND")
-            
-    print(f"{clocktime()}\tBULK\tDOWNLOAD\tEND")
+                # Check if the scene was found...
+                if len(items) != 0:
+                    print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tFOUND")
+                
+                    resolution = int(args.resolution)
+                
+                    try:
+                        imagery = stac_load(
+                            items, 
+                            #bands=["B02","B03","B04","B05","B06","B07","B08","B11","B12","SCL"],
+                            bands=["SCL"],
+                            chunks={"x": 2048, "y": 2048}, 
+                            stac_cfg=cfg,
+                            patch_url = pc.sign,
+                            resolution=resolution,
+                        )
+                        bands = list(imagery.data_vars)
+                
+                        print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tDOWNLOAD")
+                        for band in bands:
+                            print(f"INFO\t{clocktime()}\t{sid}\t{band}\tSTART")
+                            output_path = os.path.join(output_folder, f"{sid}_{band}.tif")
+                            to_float(imagery.get(band)).compute().odc.write_cog(output_path, overwrite=True)
+                            print(f"INFO\t{clocktime()}\t{sid}\t{band}\tEND")
+                        print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tDONE")
+                        
+                    except Exception as e:
+                        print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tERROR")
+                else:
+                    print(f"INFO\t{clocktime()}\t{sid}\tSCENE\tNOT FOUND")
+                    
+            print(f"{clocktime()}\tBULK\tDOWNLOAD\tEND")
 
