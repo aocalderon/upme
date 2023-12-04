21060dae6862157c0e142c96aa6e06c61f3c4fb2
Andres Calderon <andress.calderon@gmail.com>
2023-12-03 17:39:27 -0800

    Sync from laptop...

--- ids_diff.R
+++ ids_diff.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
 ids1 <- read_tsv("missing_bands_v1.tsv", col_names = F) 
-ids2 <- read_tsv("missing_bands_solved_v2.tsv", col_names = F) 
+ids2 <- read_tsv("missing_bands_solved_v3.tsv", col_names = F) 
 
-setdiff(ids1, ids2) |> write_tsv("missing_bands_v2.tsv", col_names = F)
+setdiff(ids1, ids2) |> write_tsv("missing_bands_v3.tsv", col_names = F)
