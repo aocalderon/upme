3f25a3be353cd15418c1e388e2040444b8eb493e
Andres Calderon <andress.calderon@gmail.com>
2023-12-03 19:42:29 -0800

    Sync from laptop...

--- ids_agg.R
+++ ids_agg.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 fields <- c("A1","A2","A3","A4","A5","A6","Band")
-read_tsv("missing_bands_v4.tsv", col_names = c("id")) |>
+read_tsv("missing_bands_v6.tsv", col_names = c("id")) |>
   separate(id, into = fields, sep = "_") |>
   mutate(id = paste0(A1,"_",A2,"_",A3,"_",A4,"_",A5,"_",A6)) |>
   select(id, Band) |>
--- ids_diff.R
+++ ids_diff.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
 ids1 <- read_tsv("missing_bands_v1.tsv", col_names = F) 
-ids2 <- read_tsv("missing_bands_solved_v4.tsv", col_names = F) 
+ids2 <- read_tsv("missing_bands_solved_v6.tsv", col_names = F) 
 
-setdiff(ids1, ids2) |> write_tsv("missing_bands_v4.tsv", col_names = F)
+setdiff(ids1, ids2) |> write_tsv("missing_bands_v6.tsv", col_names = F)
