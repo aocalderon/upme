3b33a3effd29ca90818336b8286faf61a8b93ec6
Andres Calderon <acald013@ucr.edu>
2023-12-12 22:43:57 -0800

    Working on pixels to points...

--- pixels2points.py
+++ pixels2points.py
@@ -1,108 +1,70 @@
-"""thread_pool_executor.py
-
-Operate on a raster dataset window-by-window using a ThreadPoolExecutor.
-
-Simulates a CPU-bound thread situation where multiple threads can improve
-performance.
-
-With -j 4, the program returns in about 1/4 the time as with -j 1.
-"""
-
-import concurrent.futures
-import multiprocessing
+import rasterio
 import numpy as np
 import pandas as pd
 
-import rasterio
+def main(infile, outpath, coords, n):
+    """Process infile block-by-block and write to a new file"""
 
-def mycompute(input):
-    f = input.flatten()
-    return f
-
-
-def main(infile, outfile, num_workers = 4):
-    """Process infile block-by-block and write to a new file
-
-    The output is the same as the input, but with band order
-    reversed.
-    """
-
-    chunk_size = 512
-    coords = True
+    filename = args.input.split(".")[0]
+    arr = filename.split("_")
+    band = arr[-1]
+    sid = "_".join(arr[:-1])
+    print(f"{sid}\t{band}")
+    print(f"n={n}")
     with rasterio.Env():
         with rasterio.open(infile) as src:
-            # Create a destination dataset based on source params. The
-            # destination will be tiled, and we'll process the tiles
-            # concurrently.
+            # Update the profile to use blocks and allowing tiling...
             profile = src.profile
-            profile.update(blockxsize = chunk_size, blockysize = chunk_size, tiled = True)
+            profile.update(blockxsize=2048, blockysize=2048, tiled=True)
+            print(profile)
 
-            # Materialize a list of destination block windows
-            # that we will use in several statements below.
-            windows = [window for ij, window in src.block_windows()]
+            with rasterio.open("/tmp/t", "w", **profile) as dst:
+                # Collect the windows based on the blocks...
+                windows = [window for ij, window in dst.block_windows()]
 
-            # This generator comprehension gives us raster data
-            # arrays for each window. Later we will zip a mapping
-            # of it with the windows list to get (window, result)
-            # pairs.
-            data_gen = (src.read(window=window) for window in windows)
+                # This generator comprehension gives us raster data arrays for each window...
+                data_gen = ( src.read(window=window) for window in windows )
 
-            for window, data in zip( windows, data_gen ):
-                vals = data.flatten()
-                cols, rows = np.meshgrid(np.arange(window.width) + window.col_off, np.arange(window.height) + window.row_off)
-                if coords:
-                    xs, ys = rasterio.transform.xy(src.transform, rows, cols)
-                    lons = np.array(xs).flatten()
-                    lats = np.array(ys).flatten()
-                    d = {"lon": lons.astype(int), "lat": lats.astype(int), "value": vals.astype(int)}
-                else:
-                    d = {"value": vals.astype(int)}
+                # Let's iterate over the windows and generator...
+                for window, data in zip( windows, data_gen ):
 
-                df = pd.DataFrame(d)
-                df = df[(df['value'] > 0.0)]
-                if df.size > 0:
-                    outfile = f"tsvs/Q_{window.col_off}_{window.row_off}.tsv"
-                    print(f"Saving {outfile}...")
-                    df.to_csv(outfile, sep = "\t", index = False)
-            
-            with concurrent.futures.ThreadPoolExecutor(max_workers = num_workers) as executor:
-                # We map the compute() function over the raster
-                # data generator, zip the resulting iterator with
-                # the windows list, and as pairs come back we
-                # write data to the destination dataset.
+                    # Create grids for each block...
+                    X = np.arange(window.width)  + window.col_off
+                    Y = np.arange(window.height) + window.row_off
+                    cols, rows = np.meshgrid(X, Y)
 
-                for window, vals in zip( windows, executor.map(mycompute, data_gen) ):
-                    cols, rows = np.meshgrid(np.arange(window.width) + window.col_off, np.arange(window.height) + window.row_off)
+                    # Get data columns from the image with coordinates if required...
                     if coords:
-                        xs, ys = rasterio.transform.xy(src.transform, rows, cols)
+                        xs, ys = rasterio.transform.xy(src.transform, rows, cols) 
+                        vals = data.flatten() # Extract pixel values...
                         lons = np.array(xs).flatten()
                         lats = np.array(ys).flatten()
-                        d = {"lon": lons.astype(int), "lat": lats.astype(int), "value": vals.astype(int)}
+                        d = {"lon": lons.astype(int), "lat": lats.astype(int), band: vals.astype(int)}
                     else:
-                        d = {"value": vals.astype(int)}
+                        vals = data.flatten() # Extract pixel values...
+                        d = {band: vals.astype(int)}
 
+                    # Create the dataframe and filter out null values...
                     df = pd.DataFrame(d)
-                    df = df[(df['value'] > 0.0)]
+
+                    #if not coords:
+                    #    df = df[(df[band] > 0.0)]
+
+                    # Save data just for valid blocks...
                     if df.size > 0:
-                        outfile = f"tsvs/T_{window.col_off}_{window.row_off}.tsv"
+                        outfile = f"{outpath}/BLOCK_{window.col_off}_{window.row_off}_{band}.tsv"
                         print(f"Saving {outfile}...")
                         df.to_csv(outfile, sep = "\t", index = False)
-
 
 if __name__ == "__main__":
 
     import argparse
 
-    parser = argparse.ArgumentParser(description="Concurrent raster processing demo")
-    parser.add_argument("input", metavar="INPUT", help="Input file name")
-    parser.add_argument("output", metavar="OUTPUT", help="Output file name")
-    parser.add_argument(
-        "-j",
-        metavar="NUM_JOBS",
-        type=int,
-        default=multiprocessing.cpu_count(),
-        help="Number of concurrent jobs",
-    )
+    parser = argparse.ArgumentParser(description="Raster translator.")
+    parser.add_argument("input", metavar="INPUT", help="Input file name.")
+    parser.add_argument("outpath", metavar="OUTPUT", help="Output path route.")
+    parser.add_argument("--coords", action=argparse.BooleanOptionalAction, help="Print coordinates.")
+    parser.add_argument("--n", type = int, default=2048, help="Side size of the block.")
     args = parser.parse_args()
 
-    main(args.input, args.output, args.j)
+    main(args.input, args.outpath, args.coords, args.n)
